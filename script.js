facd6c134663efbe20954c2e449a468c53531264
devangb5 <devangb5@gmail.com>
2024-11-13 23:25:24 -0500

    improve

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ document.getElementById('closeOverlay').addEventListener('click', function () {
 });
 
 
+document.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+      header.classList.add('scrolled');
+  } else {
+      header.classList.remove('scrolled');
+    }
+  });
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -32,4 +42,19 @@ document.addEventListener("DOMContentLoaded", function () {
           });
       });
   });
+});
+document.getElementById('toggleBtn').addEventListener('click', function () {
+  // Select all content items
+  const contentItems = document.querySelectorAll('.content-container .content-item');
+  const isShowingMore = this.innerText === 'Show Less';
+
+  // Toggle display based on the button state
+  contentItems.forEach((item, index) => {
+      if (index >= 6) {
+          item.style.display = isShowingMore ? 'none' : 'block';
+      }
+  });
+
+  // Toggle button text
+  this.innerText = isShowingMore ? 'Show More' : 'Show Less';
 });
